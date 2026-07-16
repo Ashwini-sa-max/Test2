102b8826c712365a1aee8cfbc5b7918aa70b3ad1
Ashwini_sa_max <ashusweety15@gmail.com>
2026-07-16 20:51:39 +0530

    button.py added

--- button.py
+++ button.py
@@ -1,8 +1,9 @@
+#Testing
 #Button
 import streamlit as st
 # Create a button
 button_clicked = st.button("Click me!")
-# Check if the button is clicked
+# Check if the button is clicked 
 if button_clicked:
     st.write("Button is clicked!")
 else:
